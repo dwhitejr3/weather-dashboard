01755a5e6b01ec0b009738a6ea1dd48b2102ddd6
DJ <root@Demetriuss-MBP.lan>
2024-04-18 10:02:01 -0500

    fixed container and search bar

---
+++ assets/homepage.js
@@ -0,0 +1,8 @@
+const searchBtn = $('.btn-primary');
+const cityList = $('.city-list');
+
+
+function createList() {
+const newCity = $('<li>');
+newCity.addClass('col-2').css('color' , 'white').css('background-color ,gray').css('border' , '5px').css('border-radius', '10px').css('text-align', 'center').css('padding', '3px 10px');
+}
